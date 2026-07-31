b5e74ab5a7eb96943653479d6a7d2e909d81414d
SHAIK THOUHID <shaikthouhid890@gmail.com>
2026-07-31 09:54:33 +0530

    Add files via upload

---
+++ DEVOPS/P4.R
@@ -0,0 +1,6 @@
+a <- as.numeric(readline(prompt = "Enter first number: "))
+b <- as.numeric(readline(prompt = "Enter second number: "))
+
+sum <- a + b
+
+print(paste("Sum =", sum))
---
+++ DEVOPS/P5.R
@@ -0,0 +1,10 @@
+a <- 10
+b <- 20
+
+temp <- a
+a <- b
+b <- temp
+
+print(paste("After swapping:"))
+print(paste("a =", a))
+print(paste("b =", b))
